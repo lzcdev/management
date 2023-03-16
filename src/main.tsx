2b26300eed88db0450ed5aa875db3fc8d10e1abe
lzcdev <lzc2134@gmail.com>
2023-03-16 20:46:17 +0800

    feat: 第一节

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 样式初始化
+import 'reset-css'
+import '@/assets/styles/global.scss'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
