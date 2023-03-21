158053b2c7b8bb367f6115fcc869b78d5c11e065
lzcdev <lzc2134@gmail.com>
2023-03-21 21:52:05 +0800

    feat: 老路由片配置方式

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 样式初始化
-import 'reset-css'
+// import 'reset-css'
 import '@/assets/styles/global.scss'
-import App from './App'
+import 'antd/dist/reset.css';
+// import App from './App'
+import Router from './router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 )
